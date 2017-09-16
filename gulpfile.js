b5a12f0eaed98b50347fadabb13eed45db0f94ca
dlemke <dlemke@live.com>
2017-09-16 12:08:19 -0500

    update gulp task to build and then serve project

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,24 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var exec = require('child_process').exec;
 
-gulp.task('default', function () {
-    nodemon({
-        script: './server.js',
-        ext: 'js',
-        env: {
-            PORT: 8000
-        },
-        ignore: ['./node_modules/**']
-    });
+gulp.task('default', ['build', 'serve']);
+
+gulp.task('build', function (cb) {
+  exec('ng build', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+gulp.task('serve', ['build'], function () {
+  nodemon({
+    script: './server.js',
+    ext: 'js',
+    env: {
+      PORT: 8000
+    },
+    ignore: ['./node_modules/**']
+  });
 });
