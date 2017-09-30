0e4bcafecfe4265a44175ec62a0263617391c22f
dlemke <dlemke@live.com>
2017-09-29 21:20:43 -0500

    cleanup/refactoring

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
 
 var options = {
     retainKeyOrder: true
